41e9479b886e9b68586c5136eab18bbd6c1e5adc
Josh Medeski <josh.medeski@gmail.com>
2023-11-01 21:35:15 -0500

    chore: remove unused code

--- lua/telescope/_extensions/smart-goto.lua
+++
@@ -1,8 +0,0 @@
-local has_telescope, telescope = pcall(require, "telescope")
-
-if not has_telescope then
-	error("telescope-smart-goto.nvim requires nvim-telescope/telescope.nvim")
-end
-return telescope.register_extension({
-	exports = { ["smart-goto"] = require("extensions.harpoon") },
-})
